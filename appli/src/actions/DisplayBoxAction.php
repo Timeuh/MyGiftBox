2d999b4f8eef7965006902c63fea5ae69f14b245
Timeuh <timeuuh@gmail.com>
2023-06-12 18:20:17 +0200

    feat: crée l'action qui permet d'afficher la box courante

---
+++ appli/src/actions/DisplayBoxAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace gift\app\actions;
+
+use gift\app\services\box\BoxService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Views\Twig;
+
+// permet l'affichage d'une box
+class DisplayBoxAction extends AbstractAction {
+
+    // méthode magique invoquée pour gérer l'action
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        // récupère l'id de la box courante en session
+        $boxId = $_SESSION['currentBox'] ?? null;
+
+        // s'il n'y a pas de box courante, lance une erreur
+        if ($boxId === null) {
+            throw new HttpBadRequestException($request,"vous n'avez pas créé de Box, créez en une avant d'y accéder !");
+        }
+
+        // récupère la box
+        $box = BoxService::getBoxById($boxId);
+        // récupère les prestations
+        $prestations = $box->prestation()->get();
+
+        // charge la vue depuis la template Twig et la retourne
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'afficherBox.twig', ['box' => $box, 'prestations' => $prestations]);
+    }
+}
