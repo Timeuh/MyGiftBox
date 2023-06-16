1af53ee85d729784c936f2c49d0d2b0b55d76842
Timeuh <timeuuh@gmail.com>
2023-06-16 10:57:51 +0200

    style: ajoute la couleur d'erreur

--- appli/tailwind.config.js
+++ appli/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       colors: {
         'green-tea': '#BEEDAA',
         'dark-tea': '#113E06',
-        'tea-green': '#D5FFD9'
+        'tea-green': '#D5FFD9',
+        'error': '#f32d52'
       }
     },
   },
