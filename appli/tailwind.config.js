f151a48d855f2e6cb681c4227b81d1589569aac6
Timeuh <timeuuh@gmail.com>
2023-06-16 11:28:25 +0200

    style: ajoute les nouvelles couleurs

--- appli/tailwind.config.js
+++ appli/tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
         'green-tea': '#BEEDAA',
         'dark-tea': '#113E06',
         'tea-green': '#D5FFD9',
-        'error': '#f32d52'
+        'error': '#f32d52',
+        'main-background': '#5f8b4f',
+        'primary': '#210D3A'
       }
     },
   },
