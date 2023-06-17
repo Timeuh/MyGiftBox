21b244ca4fa765e09527f712ceb767a38b1b9fd2
Timothée Brindejonc <96261549+Timeuh@users.noreply.github.com>
2023-06-17 21:14:59 +0200

    Feature/nouveau style (#41)
    * feat: ajoute l'icône du site
    
    * style: ajoute les couleurs de base
    
    * style: restyle la navbar
    
    * refactor: bouge le style de la navbar
    
    * style: ajoute une nuance de green-tea
    
    * style: restyle la barre de crédit
    
    * style: ajoute la couleur d'erreur
    
    * style: restyle la page d'erreur
    
    * style: fait prendre la pleine largeur à la section main
    
    * style: ajoute les nouvelles couleurs
    
    * style: restyle la home page
    
    * style: restyle la page de connexion
    
    * style: restyle la page d'enregistrement
    
    * style(refactor): rend le footer fixed pour qu'il suive le bas de l'écran
    
    * style: restyle l'affichage de détail d'une box
    
    * style: restyle l'affichage de détail d'une box validée
    
    * style: restyle l'affichage des box par défaut
    
    * style: restyle l'affichage de la box finie
    
    * style: restyle l'affichage de la page de confirmation de paiement
    
    * style: restyle l'affichage de la page de résultat de paiement
    
    * style: restyle l'affichage de la page "mes box"
    
    * style: restyle l'affichage de la page d'ajout d'une prestation à une box
    
    * style: restyle l'affichage de la page de confirmation de création de box
    
    * style: restyle l'affichage de la page de création de box
    
    * style: restyle l'affichage de la page de création de box à partir d'une template
    
    * style: restyle l'affichage de la page de présentation de prestation
    
    * delete: supprime la page de création de catégorie, non demandé
    
    * style: restyle la page d'affichage des prestations d'une catégorie
    
    * style: restyle la page d'affichage des catégories
    
    * style: restyle la page d'affichage d'une prestation
    
    * style: restyle la page d'affichage de message de validation d'une box
    
    * style(fix): arrange l'espacement pour voir tous les éléments de la page

--- appli/tailwind.config.js
+++ appli/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
       "./src/templates/*.twig"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-tea': '#BEEDAA',
+        'dark-tea': '#113E06',
+        'tea-green': '#D5FFD9',
+        'error': '#f32d52',
+        'main-background': '#5f8b4f',
+        'primary': '#210D3A'
+      }
+    },
   },
   plugins: [],
 }
